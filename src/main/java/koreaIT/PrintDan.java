39c5409d5579bc01e2d0d7b8e94c4e831875b3cd
rrohyo <aoslwjs2926@gmail.com>
2025-07-15 20:00:26 +0900

    jsp 활용

--- src/main/java/koreaIT/PrintDan.java
+++ src/main/java/koreaIT/PrintDan.java
@@ -13,34 +13,7 @@ public class PrintDan extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		response.setContentType("text/html;charset=UTF-8");
-		
-		String inputDan = request.getParameter("dan");
-		String inputLimit = request.getParameter("limit");
-		String color = request.getParameter("color");
-		System.out.println(inputDan);
-		System.out.println(inputLimit);
-		System.out.println(color);
-		
-
-		if (inputDan == null) {
-			inputDan = "2";
-		}
-		if (inputLimit == null) {
-			inputLimit = "1";
-		}
-		
-		
-		int dan = Integer.parseInt(inputDan);
-		int limit = Integer.parseInt(inputLimit);
-		
-		response.getWriter().append(String.format("<h1 style='color':%s;>== %d 단 ==</h1>", color, dan));
-			
-
-		for (int i = 1; i <= limit; i++) {
-			response.getWriter().append(String.format("%d * %d = %d<br>", dan, i, (dan * i)));
-
-		}
+		request.getRequestDispatcher("/jsp/home/printDan.jsp").forward(request, response);
 
 	}
 
