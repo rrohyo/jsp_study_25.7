15d6ee9c9fb3a17fd4ad7046b307d2508d3d0ebd
rrohyo <aoslwjs2926@gmail.com>
2025-07-22 20:26:59 +0900

    목록에서부터 수정할 수 있도록 경로지정 구현

--- src/main/java/koreaIT/servlet/ArticleListServlet.java
+++ src/main/java/koreaIT/servlet/ArticleListServlet.java
@@ -27,7 +27,7 @@ public class ArticleListServlet extends HttpServlet {
 
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			String url = "jdbc:mysql://localhost:3306/jdbc_db?useUnicode=true&characterEncoding=UTF-8&autoReconnect=true&serverTimezone=Asia/Seoul";
+			String url = "jdbc:mysql://localhost:3306/jdbc_db?useUnicode=true&characterEncoding=UTF-8&serverTimezone=Asia/Seoul";
 			conn = DriverManager.getConnection(url, "root", "1234");
 			System.out.println("연결 성공!");
 			
@@ -39,13 +39,13 @@ public class ArticleListServlet extends HttpServlet {
 			if (request.getParameter("page") != null && request.getParameter("page").length() != 0) {
 				page = Integer.parseInt(request.getParameter("page"));
 			}
-			System.out.println("page" + page);
+			System.out.println("page : " + page);
 
 			int itemsInAPage = 10;
 			int limitFrom = (page - 1) * itemsInAPage;
 			
-			System.out.println("itemsInAPage" + itemsInAPage);
-			System.out.println("limitFrom" + limitFrom);
+			System.out.println("itemsInAPage : " + itemsInAPage);
+			System.out.println("limitFrom : " + limitFrom);
 			
             DBUtil dbUtil = new DBUtil(request, response);
             
@@ -61,13 +61,13 @@ public class ArticleListServlet extends HttpServlet {
             sql.append("SELECT *");
             sql.append("FROM `article`");
             sql.append("ORDER BY `id` DESC");
-            sql.append("limit ?, ?", limitFrom, itemsInAPage);
+            sql.append("LIMIT ?, ?", limitFrom, itemsInAPage);
             
             List<Map<String, Object>> articleRows = dbUtil.selectRows(conn, sql);
             
             
-            System.out.println("totalCnt"+totalCnt);
-			System.out.println("totalPage"+totalPage);
+            System.out.println("totalCnt : "+totalCnt);
+			System.out.println("totalPage : "+totalPage);
             
             request.setAttribute("articleRows", articleRows); // jsp에 데이터를 넘겨준다.
             request.setAttribute("page", page); 
